b4eeebb0260a8dd596f9d15b1477031f0101a8e1
Dieter Holstein <holstein.dieter@web.de>
2023-07-04 10:52:16 +0000

    changes

--- demo/src/main/java/com/example/demo/TodoController.java
+++ demo/src/main/java/com/example/demo/TodoController.java
@@ -1,11 +1,15 @@
 package com.example.demo;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/todos")
 public class TodoController {
